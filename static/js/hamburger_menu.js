b3fe82c90afa769dcb62214e9e3463918973b13d
Tymur <tymurkulivar@gmail.com>
2025-02-01 12:31:27 +0100

    add auto closing of the dialog on large devices

--- static/js/hamburger_menu.js
+++ static/js/hamburger_menu.js
@@ -9,4 +9,14 @@ document.addEventListener('DOMContentLoaded', function () {
     hamburger2.addEventListener('click', function () {
         navMenu.close()
     });
+    window.addEventListener('resize', function () {
+        if (window.innerWidth > 800) {
+            navMenu.close();
+        }
+    });
+});
+
+document.querySelector('.login_toggle_menu').addEventListener('click', function () {
+    document.querySelector('.list').classList.toggle('hidden');
+    document.getElementById('login_img').classList.toggle('active');
 });
