faa2566578d93756bba465486137cad5ab29553a
Tymur <tymurkulivar@gmail.com>
2025-02-01 12:28:28 +0100

    add size constraints for the automatic 'hover'

--- static/js/hover_on_center.js
+++ static/js/hover_on_center.js
@@ -4,9 +4,9 @@ export function hover_on_center(cards, isSingleColumn){
     const topInCenter = rect.top >= window.innerHeight / 2 - rect.height * 1.3
     const bottomInCenter = rect.bottom <= window.innerHeight / 2 + rect.height * 1.3
     const isInCenter = topInCenter && bottomInCenter
-    // console.log(isInCenter, isSingleColumn)
+    const isInMobileMode = window.innerWidth <= 800
 
-    if (isInCenter && isSingleColumn) {
+    if (isInCenter && isSingleColumn && isInMobileMode) {
       cart.classList.add('hover');
     } else {
       cart.classList.remove('hover');
