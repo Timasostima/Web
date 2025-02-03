e144fd2529d10b0c9e20b8a22c4f964843b649ed
Tymur <tymurkulivar@gmail.com>
2025-02-03 12:35:53 +0100

    prevent autofill on registration

--- app/forms.py
+++ app/forms.py
@@ -6,11 +6,16 @@ from app.models import User
 
 class RegisterForm(FlaskForm):
 
-    email = EmailField(validators=[InputRequired(), Length(min=4, max=30), Email()], render_kw={"placeholder": "Email"})
-    name = StringField(validators=[InputRequired(), Length(min=3, max=20)], render_kw={"placeholder": "Name"})
-    last_name = StringField(validators=[Optional(), Length(min=3, max=30)], render_kw={"placeholder": "Lastname"})
-    company_name = StringField(validators=[Optional(), Length(min=3, max=20)], render_kw={"placeholder": "Company Name"})
-    password = PasswordField(validators=[InputRequired(), Length(min=6, max=20)], render_kw={"placeholder": "Password"})
+    email = EmailField(validators=[InputRequired(), Length(min=4, max=30), Email()],
+                       render_kw={"placeholder": "Email"})
+    name = StringField(validators=[InputRequired(), Length(min=3, max=20)],
+                       render_kw={"placeholder": "Name"})
+    last_name = StringField(validators=[Optional(), Length(min=3, max=30)],
+                            render_kw={"placeholder": "Lastname"})
+    company_name = StringField(validators=[Optional(), Length(min=3, max=20)],
+                               render_kw={"placeholder": "Company Name"})
+    password = PasswordField(validators=[InputRequired(), Length(min=6, max=20)],
+                             render_kw={"placeholder": "Password", "autocomplete": "new-password"})
     submit = SubmitField('Register')
 
     def validate_email(self, email):
