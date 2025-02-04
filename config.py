cdaef6eb7470a0c545af61765302db2d71f9b910
Tymur <tymurkulivar@gmail.com>
2025-02-04 22:59:32 +0100

    use .env files to hide confedential data

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv('variables.env')
 
 
 class Config:
-    SECRET_KEY = 'your_secret_key'
+    SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///travel_routes.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
